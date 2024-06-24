98e77a8d843ed641b9d555e18e0145bc97140356
Dennis Dam <dend@flemingcollege.ca>
2024-06-23 22:01:49 -0400

    Update Lab4.py
    the beginning of the script with the file path of the script.
    
    Co-Authored-By: Francis <9560564+Baker@users.noreply.github.com>
    Co-Authored-By: as <8127015+as@users.noreply.github.com>
    Co-Authored-By: my <50836+my@users.noreply.github.com>

---
+++ Lab4.py
@@ -0,0 +1,7 @@
+#### Name: Dennis Dam ######
+#### School fleming College #####
+##### project: writing a python script that process a text file line by line #####
+import re
+import sys
+def get_log_file_log_path(/Users/dennidam/Desktop/gateway.log):
+    if 
