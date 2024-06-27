20cfec17a3feb3ba172bffbce67f83ad01934f48
Dennis Dam <dend@flemingcollege.ca>
2024-06-27 14:17:18 -0400

    Update Lab4.py
    Co-Authored-By: Francis <9560564+Baker@users.noreply.github.com>

--- Lab4.py
+++ Lab4.py
@@ -1,7 +1,10 @@
-#### Name: Dennis Dam ######
-#### School fleming College #####
+#Name: Dennis Dam 
+#School fleming College#
+#Course: Scripting Applications# 
+
+
+
 ##### project: writing a python script that process a text file line by line #####
 import re
 import sys
-def get_log_file_log_path(/Users/dennidam/Desktop/gateway.log):
-    if 
+
