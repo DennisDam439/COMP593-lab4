f9c0ea5b21fc6c0107536eb5ac1515df63d2ae55
Dennis Dam <dend@flemingcollege.ca>
2024-07-03 08:40:37 -0400

    update .DS_store

--- lab4_script_template.py
+++ lab4_script_template.py
@@ -1,7 +1,3 @@
-#### name of student - Dennis Dam ####
-#### course - scrpting applications ##### 
-#####lab 4 gateway log.#######
-
 
 import sys
 import os
@@ -14,20 +10,7 @@ def get_log_file_path_from_cmd_line():
     return
 
 # TODO: Steps 4-7
-def filter_log_by_regex(log_file, regex, ignore_case=True, print_summary=False, print_records=False):
-    """Gets a list of records in a log file that match a specified regex.
 
-    Args:
-        log_file (str): Path of the log file
-        regex (str): Regex filter
-        ignore_case (bool, optional): Enable case insensitive regex matching. Defaults to True.
-        print_summary (bool, optional): Enable printing summary of results. Defaults to False.
-        print_records (bool, optional): Enable printing all records that match the regex. Defaults to False.
-
-    Returns:
-        (list, list): List of records that match regex, List of tuples of captured data
-    """
-    return
 
 # TODO: Step 8
 def tally_port_traffic(log_file):
